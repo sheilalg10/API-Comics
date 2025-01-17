e543972ce87e75dc67411eca3ba5852f6dcff84b
Sheila Lara Garcia <78981628+sheilalg10@users.noreply.github.com>
2025-01-17 12:27:12 +0100

    Delete apiComics/data directory

--- apiComics/data/data.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const saveToDataBase = (comic) =>{
-    fs.writeFileSync("./src/data/comics.json",JSON.stringify(comic,null,2),{
-        encoding:"utf-8"
-    })
-}
-
-module.exports = saveToDataBase;
